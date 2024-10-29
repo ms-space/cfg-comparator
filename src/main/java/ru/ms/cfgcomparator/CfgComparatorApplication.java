923671cceb6a9118c76e4c6c4ddf82032f5d5f10
Maksim Shibanov <asusib74@gmail.com>
2024-10-29 14:27:39 +0500

    init

---
+++ src/main/java/ru/ms/cfgcomparator/CfgComparatorApplication.java
@@ -0,0 +1,13 @@
+package ru.ms.cfgcomparator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfgComparatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfgComparatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ms/cfgcomparator/CfgComparatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ms.cfgcomparator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfgComparatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
